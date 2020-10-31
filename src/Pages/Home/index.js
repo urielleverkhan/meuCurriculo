83a868434e9030a1aa21abbcf78cd319285cca41
Naira Magalhães <nairadanile@gmail.com>
2020-10-31 16:00:26 -0300

    -Adicionado fundo que muda de cor

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -12,32 +12,73 @@ class Home extends Component{
         super(props);
         this.state ={
             corFundo: 'fff',
-            porcento: 0
         };
-        this.porcentagem = this.porcentagem.bind(this);
+        this.handleScroll = this.handleScroll.bind(this);
     }
     handleScroll() { 
-        if (document.documentElement.scrollTop > 1500) {
+        let state = this.state;
+        let tamanhoTela = document.body.clientHeight;
+        let telaAgora = document.documentElement.scrollTop;
+        let porcents = (telaAgora / tamanhoTela)*100;
+        
+
+        console.log(porcents);
+        console.log(telaAgora);
+
+        if (porcents > 0 && porcents <= 10) {
            this.setState({
-             corFundo: '4c3c4a'
+             corFundo: 'c79fd8'
            })
-           console.log(document.documentElement.scrollTop);
          } 
-        else if (document.documentElement.scrollTop < 500){
+         else if (porcents > 10 && porcents <= 20){
             this.setState({
-                corFundo: 'eae5e5'
+                corFundo: 'be94d8'
+              })
+        }
+        else if (porcents > 20 && porcents <= 30){
+            this.setState({
+                corFundo: 'b589d5'
+              })
+        }
+        else if (porcents > 30 && porcents <= 40){
+            this.setState({
+                corFundo: 'ab7ed2'
+              })
+        }
+        else if (porcents > 40 && porcents <= 50){
+            this.setState({
+                corFundo: 'a174cf'
+              })
+        }
+        else if (porcents > 50 && porcents <= 60){
+            this.setState({
+                corFundo: '9564c5'
+              })
+        }
+        else if (porcents > 60 && porcents <= 70){
+            this.setState({
+                corFundo: '8953b8'
+              })
+        }
+        else if (porcents > 70 && porcents <= 80){
+            this.setState({
+                corFundo: '7e43b1'
+              })
+        }
+        else if (porcents > 80 && porcents <= 90){
+            this.setState({
+                corFundo: '7231a7'
+              })
+        }
+        else if (porcents > 90 && porcents <= 100){
+            this.setState({
+                corFundo: '5c2994'
               })
         }
 
        }
-       porcentagem(){
-        console.log(window.innerHeight);
-        console.log(document.innerHeight);
-       }
-     
        componentDidMount() {
-         window.onscroll = () => this.handleScroll();
-         
+         window.onscroll = () => this.handleScroll();  
        }
 
       
