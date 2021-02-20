a9b9d9d756d86e79830fdfc00964cd0766328d79
Naira Magalhães <52166063+urielleverkhan@users.noreply.github.com>
2021-02-20 11:08:04 -0300

    -site arrumado ate a resolucao 700px

--- src/Pages/Sobre/index.js
+++ src/Pages/Sobre/index.js
@@ -13,23 +13,25 @@ class Sobre extends Component{
                     <h1>Sobre</h1>
                     <h3>Minha historia</h3>
                     <p> Apaixonada desde muito pequena por computação e em especial programação, 
-                        me formei no curso de Engenharia da computação
-                        pela UNORP - Universidade do norte paulista. </p>
-                    <p>Gosto muito de estudar, e muito do que aprendi de programação,
-                        foi em cursos onlines feitos antes ou apos a faculdade, tenho conta na Udemy e Digital Innovation
-                        com muitos certificados, dente eles muitos estão aqui neste curriculo. </p>
-                    <p>Apos terminar a faculdade cai de cabeça no estudo de desenvolvimento front-end e, 
-                        aprendi muito de css e html quando fiz estagio em uma empresa
-                        de desenvolvimento de sites na cidade de São José do Rio preto interior de São paulo
-                        onde morei minha vida toda, agora estou me aprofundando em Javascript e React-Js.
+                        me formei no curso de Engenharia da Computação, pela 
+                        UNORP - Universidade do Norte Paulista.</p>
+                    <p>Gosto muito de estudar, e muito do que aprendi de programação foi em
+                         cursos online feitos antes ou após a faculdade. Tenho conta na Udemy 
+                         e Digital Innovation com muitos certificados, dentre eles vários estão 
+                         aqui neste currículo.</p>
+                    <p>Após terminar a faculdade, caí de cabeça no estudo de desenvolvimento front-end.
+                         Aprendi muito de css e html quando fiz estágio em uma empresa de desenvolvimento 
+                         de sites na cidade de São José do Rio Preto, interior de São Paulo, onde morei 
+                         durante minha vida toda. Agora estou me aprofundando em Javascript e React-Js.
+
                     </p>
-                    <p>No final do ano de 2020 tive a oportunidade de participar de um bootcamp FullStack organizado pela 
-                        Digital Innovation One. Aprendi muito neste bootcamp e dentre as coisas mais relevantes que aprendi foi
-                        AngularJs e o desenvolvimento de apis back-end na linguagem java Spring boot com jdbc/jpa.
+                    <p>No final do ano de 2020, tive a oportunidade de participar de um bootcamp 
+                        FullStack organizado pela Digital Innovation One. Aprendi muito neste 
+                        bootcamp e dentre as coisas mais relevantes que aprendi foram AngularJs 
+                        e o desenvolvimento de apis back-end na linguagem java Spring boot com jdbc/jpa.
                     </p>
-                    <p>Hoje me mudei para a capital do estado de São Paulo de mesmo nome atraz de novas
-                        experiencias e crescimento profissional. 
-                    </p>
+                    <p>Agora estou morando na capital do estado de São Paulo, de mesmo nome, 
+                        atrás de novas experiências e crescimento profissional.               </p>
                     </Container>
             </Container>
         ); 
