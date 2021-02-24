69cb93bbdc643794ffd51191af1893dd59f3d103
Naira Magalhães <52166063+urielleverkhan@users.noreply.github.com>
2021-02-24 15:10:11 -0300

    -construido habilidades

--- src/Pages/habilidadesIdiomas/index.js
+++ src/Pages/habilidadesIdiomas/index.js
@@ -16,10 +16,19 @@ class habilidadesIdiomas extends Component{
       
         return(
             <Container fluid className="habilidadesInteiro">
-                <Container fluid className="item"><p className="itemDescricao">JavaScript: </p><BarraDeExperiencia propsLinhaGrossa={this.props.propsClassLinhaGrossa} propsPorcentagem="99%"/></Container>
-                <BarraDeExperiencia propsLinhaGrossa={this.props.propsClassLinhaGrossa} propsPorcentagem="40%"/>
-                <BarraDeExperiencia propsLinhaGrossa={this.props.propsClassLinhaGrossa} propsPorcentagem="10%"/>
-              
+                    <Container fluid className="itens">
+                        <h1>Front-end</h1>
+                        <Container fluid className="item"><p className="itemDescricao">HTML: </p><BarraDeExperiencia propsLinhaGrossa={this.props.propsClassLinhaGrossa} propsPorcentagem="95%"/></Container>
+                        <Container fluid className="item"><p className="itemDescricao">CSS: </p><BarraDeExperiencia propsLinhaGrossa={this.props.propsClassLinhaGrossa} propsPorcentagem="90%"/></Container>
+                        <Container fluid className="item"><p className="itemDescricao">JavaScript: </p><BarraDeExperiencia propsLinhaGrossa={this.props.propsClassLinhaGrossa} propsPorcentagem="70%"/></Container>
+                        <Container fluid className="item"><p className="itemDescricao">React: </p><BarraDeExperiencia propsLinhaGrossa={this.props.propsClassLinhaGrossa} propsPorcentagem="70%"/></Container>
+                        <Container fluid className="item"><p className="itemDescricao">Angular: </p><BarraDeExperiencia propsLinhaGrossa={this.props.propsClassLinhaGrossa} propsPorcentagem="40%"/></Container>
+                        <h1>Back-End</h1>
+                        <Container fluid className="item"><p className="itemDescricao">Bando de Dados: </p><BarraDeExperiencia propsLinhaGrossa={this.props.propsClassLinhaGrossa} propsPorcentagem="60%"/></Container>
+                        <Container fluid className="item"><p className="itemDescricao">Java: </p><BarraDeExperiencia propsLinhaGrossa={this.props.propsClassLinhaGrossa} propsPorcentagem="40%"/></Container>
+                        <h1>Linguas estrangeiras</h1>
+                        <Container fluid className="item"><p className="itemDescricao">Ingles: </p><BarraDeExperiencia propsLinhaGrossa={this.props.propsClassLinhaGrossa} propsPorcentagem="50%"/></Container>
+                    </Container>
             </Container>
         ); 
     }
